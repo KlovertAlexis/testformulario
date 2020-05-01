81eadaca56741e7d029c03aeaeade6972e401214
klovertalexis <klovertalexis@gmail.com>
2020-05-01 15:43:56 -0500

    primer commit de mi siteweb

---
+++ js/form.js
@@ -0,0 +1,41 @@
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  let telefono = "573105010573";
+
+  let cliente = document.querySelector("#cliente").value;
+  let fecha = document.querySelector("#fecha").value;
+  let hora = document.querySelector("#hora").value;
+  let pollo = document.querySelector("#pollo").value;
+  let bebida = document.querySelector("#bebida").value;
+  let resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  let url = `https://api.whatsapp.com/send?phone=#agregarnumero&text=
+		*_MI NEGOCIO_*%0A
+		*Reservas*%0A%0A
+		*¿Cuál es tu nombre?*%0A
+		${cliente}%0A
+		*Indica la fecha de tu reserva*%0A
+		${fecha}%0A
+		*Indica la hora de tu reserva*%0A
+		${hora}%0A
+		*Porcion elegida*%0A
+		${pollo}%0A
+		*Bebida seleccionada*%0A
+		${bebida}`;
+
+  if (cliente === "" || fecha === "" || hora === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
+
+  window.open(url);
+});
